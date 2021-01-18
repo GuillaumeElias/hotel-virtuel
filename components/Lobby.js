b9ecfd4de82fe8173c97c68c6ff1033a52ff64f8
Guillaume Elias <lefrise@gmail.com>
2021-01-18 19:03:40 +0100

    Added lobby

--- components/Lobby.js
+++ components/Lobby.js
@@ -1,11 +1,44 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Stage, Layer } from "react-konva";
 
-const Lobby = ({ history }) => (
-  <div>
-    <h2>Lobby</h2>
-    <Link to={"/reception"}>Reception</Link>
-  </div>
-);
+import CanvasImage from "./ui/CanvasImage";
+
+const Lobby = ({ history }) => {
+  return (
+    <div>
+      <Stage width={window.innerWidth} height={window.innerHeight * 1.2}>
+        <Layer>
+          <CanvasImage
+            src="/images/lobby/bar"
+            x={0}
+            y={0}
+            width={window.innerWidth / 1.5}
+            onClick={() => {
+              history.push("/frontdesk");
+            }}
+          />
+          <CanvasImage
+            src="/images/lobby/manandplant"
+            x={0}
+            y={window.innerWidth / 3}
+            width={window.innerWidth / 4.2}
+            onClick={() => {
+              history.push("/frontdesk");
+            }}
+          />
+          <CanvasImage
+            src="/images/lobby/plant"
+            x={window.innerWidth / 3}
+            y={window.innerWidth / 3}
+            width={window.innerWidth / 3}
+            onClick={() => {
+              history.push("/frontdesk");
+            }}
+          />
+        </Layer>
+      </Stage>
+    </div>
+  );
+};
 
 export default Lobby;
