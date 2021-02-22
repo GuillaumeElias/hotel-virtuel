6c6ea68884758ca0550d977d69fe77c06887e871
Guillaume Elias <lefrise@gmail.com>
2021-02-22 15:25:50 +0100

    Added doors

--- components/Floors/Floor1.js
+++ components/Floors/Floor1.js
@@ -1,10 +1,56 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Stage, Layer } from "react-konva";
 
-const Floor1 = ({ match }) => (
+import CanvasImage from "../ui/CanvasImage";
+import ScrollTop from "../utils/ScrollTop.js";
+
+import { windowWidth } from "../utils/screen.js";
+
+const Floor1 = ({ history }) => (
   <div>
-    <h3>1</h3>
-    <Link to="/escalator/1">Escalier magnétique</Link>
+    <ScrollTop />
+    <Stage width={windowWidth} height={windowWidth * 0.8}>
+      <Layer>
+        <CanvasImage
+          src="/images/floor1/room101"
+          x={windowWidth / 8}
+          y={0}
+          width={windowWidth * 0.3}
+          onClick={() => {
+            history.push("/room/101");
+          }}
+        />
+
+        <CanvasImage
+          src="/images/floor1/room102"
+          x={windowWidth / 2}
+          y={0}
+          width={windowWidth * 0.3}
+          onClick={() => {
+            history.push("/room/101");
+          }}
+        />
+
+        <CanvasImage
+          src="/images/elevator"
+          x={windowWidth / 8}
+          y={windowWidth * 0.4}
+          width={windowWidth * 0.2}
+          onClick={() => {
+            history.push("/elevator/1");
+          }}
+        />
+        <CanvasImage
+          src="/images/escalator"
+          x={windowWidth / 2}
+          y={windowWidth * 0.4}
+          width={windowWidth * 0.3}
+          onClick={() => {
+            history.push("/escalator/1");
+          }}
+        />
+      </Layer>
+    </Stage>
   </div>
 );
 
